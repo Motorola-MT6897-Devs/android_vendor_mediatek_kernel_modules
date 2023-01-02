5d9402a63b2d7b88d6fa4288113ccb1a7f6f7e1f
xuemei.yang <xuemei.yang@mediatek.com>
2023-01-02 21:30:23 +0800

    [ALPS05366499] Fix reboot bt driver crash issue
    [discription]
    Root cause: when platform reboot, bt driver will do probe function,
    it will register interrupt function to sdio host driver, then create
    main_thread, maybe has some interrupts on sdio bus, it will run the
    btmtk_interrupt to wakeup the wait queue of main_thread, but At this time
    the main_thread still not created, it will access illegal address and
    cause crash.
    Solution: move register intterrupt function to after create main_thread
    
    [test]
    test ok
    
    MTK-Commit-Id: d04e1c9edd15387e5f64fb5c91293a3d40319c85
    
    Change-Id: Ic03cad3653a938214da8dfad877fe09d580a08f0
    Signed-off-by: xuemei.yang <xuemei.yang@mediatek.com>
    CR-Id: ALPS05366499
    Feature: BT Chipset Capability

--- btmtk_sdio.c
+++ btmtk_sdio.c
@@ -5382,23 +5382,6 @@ static int btmtk_sdio_probe(struct sdio_func *func,
 	btmtk_sdio_initialize_cfg_items();
 	btmtk_sdio_load_setting_files(g_card->bt_cfg_file_name, &g_card->func->dev, g_card);
 
-	BTMTK_DBG("func device %X, call btmtk_sdio_register_dev", g_card->func->device);
-	if (btmtk_sdio_register_dev(g_card) < 0) {
-		BTMTK_ERR("Failed to register BT device!");
-		return -ENODEV;
-	}
-
-	BTMTK_DBG("btmtk_sdio_register_dev success");
-
-	/* Disable the interrupts on the card */
-	btmtk_sdio_enable_host_int(g_card);
-	BTMTK_DBG("call btmtk_sdio_enable_host_int done");
-
-	if (btmtk_sdio_download_fw(g_card)) {
-		BTMTK_ERR("Downloading firmware failed!");
-		fw_download_fail = 1;
-	}
-
 	/* check buffer mode */
 	btmtk_eeprom_bin_file(g_card);
 
@@ -5418,6 +5401,24 @@ static int btmtk_sdio_probe(struct sdio_func *func,
 	}
 	BTMTK_DBG("btmtk_add_card success");
 	BTMTK_DBG("assign priv done");
+
+	BTMTK_DBG("func device %X, call btmtk_sdio_register_dev", g_card->func->device);
+	if (btmtk_sdio_register_dev(g_card) < 0) {
+		BTMTK_ERR("Failed to register BT device!");
+		return -ENODEV;
+	}
+
+	BTMTK_DBG("btmtk_sdio_register_dev success");
+
+	/* Disable the interrupts on the card */
+	btmtk_sdio_enable_host_int(g_card);
+	BTMTK_DBG("call btmtk_sdio_enable_host_int done");
+
+	if (btmtk_sdio_download_fw(g_card)) {
+		BTMTK_ERR("Downloading firmware failed!");
+		fw_download_fail = 1;
+	}
+
 	/* Initialize the interface specific function pointers */
 	pf_sdio_reset = (sdio_reset_func) btmtk_kallsyms_lookup_name("sdio_reset_comm");
 	if (!pf_sdio_reset && is_mt7668(g_card)) {
